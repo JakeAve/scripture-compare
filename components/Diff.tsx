cdd0c70ac5c32cd7f90a39edeb55d12c84669294
JakeAve <jacobjavery@gmail.com>
2024-09-23 22:51:29 -0600

    update to algorithm

--- components/Diff.tsx
+++ components/Diff.tsx
@@ -1,6 +1,6 @@
 import { JSX } from "preact/jsx-runtime";
 import { diff } from "../lib/diff.ts";
-import { isPunctuation, splitText } from "../lib/textHelpers.ts";
+import { insertSpaceBetween, splitText } from "../lib/textHelpers.ts";
 
 interface Verse {
     chapter: number;
@@ -13,7 +13,7 @@ export interface Reference {
     verses: Verse[];
 }
 
-interface Props {
+export interface DiffProps {
     reference1: Reference;
     intro1?: Verse[];
     outro1?: Verse[];
@@ -22,7 +22,10 @@ interface Props {
     outro2?: Verse[];
 }
 
-export function Diff(props: Props) {
+const side1Color = "bg-blue-200";
+const side2Color = "bg-green-200";
+
+export function Diff(props: DiffProps) {
     const { reference1, intro1, outro1, reference2, intro2, outro2 } = props;
 
     const verses1 = reference1.verses;
@@ -33,56 +36,56 @@ export function Diff(props: Props) {
 
     const d = diff(text1, text2);
 
-    const intro1Content = (intro1 || []).map(({ chapter, verse, text }) => (
-        <p class="col-start-1 col-span-1">
-            <span class="font-medium">{chapter}:{verse}{" "}</span>
-            <span class="bg-blue-200">{text}</span>
-        </p>
-    ));
+    // Deno.writeTextFileSync("test.json", JSON.stringify(d, null, 4));
 
-    const outro1Content = (outro1 || []).map(({ chapter, verse, text }) => (
-        <p class="col-start-1 col-span-1">
-            <span class="font-medium">{chapter}:{verse}{" "}</span>
-            <span class="bg-blue-200">{text}</span>
-        </p>
-    ));
+    let row1 = 1;
+    let row2 = 1;
 
-    const intro2Content = (intro2 || []).map(({ chapter, verse, text }) => (
-        <p class="col-start-2 col-span-1">
-            <span class="font-medium">{chapter}:{verse}{" "}</span>
-            <span class="bg-green-200">{text}</span>
-        </p>
-    ));
+    const intro1Content = (intro1 || []).map(({ chapter, verse, text }) => {
+        row1++;
+        row2++;
+        return (
+            <p class="col-start-1 col-span-1" style={{ gridRow: row1 }}>
+                <span class="font-medium">{chapter}:{verse}{" "}</span>
+                <span class={side1Color}>{text}</span>
+            </p>
+        );
+    });
 
-    const outro2Content = (outro2 || []).map(({ chapter, verse, text }) => (
-        <p class="col-start-2 col-span-1">
-            <span class="font-medium">{chapter}:{verse}{" "}</span>
-            <span class="bg-green-200">{text}</span>
-        </p>
-    ));
+    const intro2Content = (intro2 || []).map(({ chapter, verse, text }) => {
+        row1++;
+        row2++;
+        return (
+            <p class="col-start-2 col-span-1" style={{ gridRow: row2 }}>
+                <span class="font-medium">{chapter}:{verse}{" "}</span>
+                <span class={side2Color}>{text}</span>
+            </p>
+        );
+    });
 
     const content: JSX.Element[] = [
-        <h2 class="text-2xl justify-self-center" key="a">{reference1.book}
-        </h2>,
-        <h2 class="text-2xl justify-self-center" key="b">{reference2.book}
-        </h2>,
+        <h2 class="text-2xl justify-self-center" key="a">{reference1.book}</h2>,
+        <h2 class="text-2xl justify-self-center" key="b">{reference2.book}</h2>,
         ...intro1Content,
         <p class="col-start-2 col-span-1"></p>,
         ...intro2Content,
     ];
 
-    let row1 = 2;
-    let row2 = 2;
     let v1Idx = 0;
     let v1: Verse | undefined = verses1[v1Idx];
     let split1: string[] | undefined = v1 ? splitText(v1.text) : undefined;
     let t1Idx = 0;
     let c1: JSX.Element[] = [];
+    let currRows1: number[] = [];
     let v2Idx = 0;
     let v2: Verse | undefined = verses2[v2Idx];
     let split2: string[] | undefined = v2 ? splitText(v2.text) : undefined;
     let t2Idx = 0;
     let c2: JSX.Element[] = [];
+    let currRows2: number[] = [];
+
+    row1++;
+    row2++;
 
     let i = 0;
     while (i < d.length) {
@@ -93,30 +96,28 @@ export function Diff(props: Props) {
         if (t.value !== t1 && t.value !== t2) {
             console.log("out of sync");
             console.log(t, t1, t2, i);
+            console.log(d.slice(Math.max(i - 5, 0), Math.min(i + 5, d.length)));
+            console.log({ split1 });
+            console.log({ split2 });
+            // break;
         }
 
         if (t.removed) {
             t1Idx++;
-            const isNextPunctuation = split1?.[t1Idx]
-                ? isPunctuation(split1?.[t1Idx])
-                : false;
             c1.push(
-                <span class="bg-blue-200">
+                <span class={side1Color}>
                     {t1}
-                    {isNextPunctuation ? "" : " "}
+                    {insertSpaceBetween(t1, split1?.[t1Idx])}
                 </span>,
             );
         }
 
         if (t.added) {
             t2Idx++;
-            const isNextPunctuation = split2?.[t2Idx]
-                ? isPunctuation(split2?.[t2Idx])
-                : false;
             c2.push(
-                <span class="bg-green-200">
+                <span class={side2Color}>
                     {t2}
-                    {isNextPunctuation ? "" : " "}
+                    {insertSpaceBetween(t2, split2?.[t2Idx])}
                 </span>,
             );
         }
@@ -124,19 +125,26 @@ export function Diff(props: Props) {
         if (t.added === false && t.removed === false) {
             t1Idx++;
             t2Idx++;
-            const isNext1Punctuation = split1?.[t1Idx] &&
-                isPunctuation(split1?.[t1Idx]);
-            const isNext2Punctuation = split2?.[t2Idx] &&
-                isPunctuation(split2?.[t2Idx]);
-            c1.push(<>{t1}{isNext1Punctuation ? "" : " "}</>);
-            c2.push(<>{t2}{isNext2Punctuation ? "" : " "}</>);
+            currRows1.push(row1);
+            currRows2.push(row2);
+            c1.push(
+                <>{t1}{insertSpaceBetween(t1, split1?.[t1Idx])}</>,
+            );
+            c2.push(
+                <>{t2}{insertSpaceBetween(t2, split2?.[t2Idx])}</>,
+            );
         }
 
         if (t1Idx === split1?.length) {
             content.push(
                 <p
                     class={`col-start-1 col-span-1`}
-                    // data-text={v1?.text}
+                    style={{
+                        gridRow: row1,
+                        // `${row1} / ${
+                        // row1 < row2 ? currRows2.at(-1) || row1 : row1
+                        //  }`,
+                    }}
                 >
                     {v1 && (
                         <>
@@ -155,13 +163,21 @@ export function Diff(props: Props) {
             split1 = v1 ? splitText(v1.text) : undefined;
             c1 = [];
             row1++;
+            if (verses1.length !== verses2.length) {
+                row1 = Math.max(row1, row2);
+            }
+            currRows1 = [];
         }
 
         if (t2Idx === split2?.length) {
             content.push(
                 <p
                     class={`col-start-2 col-span-1`}
-                    // data-text={v2?.text}
+                    style={{
+                        gridRow: `${row2} / ${
+                            row2 < row1 ? currRows1.at(-1) || row2 : row2
+                        }`,
+                    }}
                 >
                     {v2 && (
                         <>
@@ -180,10 +196,25 @@ export function Diff(props: Props) {
             split2 = v2 ? splitText(v2.text) : undefined;
             c2 = [];
             row2++;
+            if (verses1.length !== verses2.length) {
+                row2 = Math.max(row1, row2);
+            }
+            currRows2 = [];
         }
 
         i++;
     }
+
+    const outro1Content = (outro1 || []).map(({ chapter, verse, text }) => {
+        row1++;
+        row2++;
+        return (
+            <p class="col-start-1 col-span-1" style={{ gridRow: row1 }}>
+                <span class="font-medium">{chapter}:{verse}{" "}</span>
+                <span class={side1Color}>{text}</span>
+            </p>
+        );
+    });
 
     content.push(
         ...outro1Content,
@@ -193,12 +224,23 @@ export function Diff(props: Props) {
         content.push(<p class="col-start-2 col-span-1"></p>);
     }
 
+    const outro2Content = (outro2 || []).map(({ chapter, verse, text }) => {
+        row1++;
+        row2++;
+        return (
+            <p class="col-start-2 col-span-1" style={{ gridRow: row2 }}>
+                <span class="font-medium">{chapter}:{verse}{" "}</span>
+                <span class={side2Color}>{text}</span>
+            </p>
+        );
+    });
+
     content.push(
         ...outro2Content,
     );
 
     return (
-        <div class="grid grid-cols-2 gap-4">
+        <div class="mx-auto grid grid-cols-2 max-w-4xl gap-4">
             {content}
         </div>
     );
